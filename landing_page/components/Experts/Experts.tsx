cddc3545d9dc79e4479b5b756068461ddeb5c1f0
kursaderduhan <kursaderduhan42@gmail.com>
2022-09-15 01:32:03 +0300

    changed experts page

--- landing_page/components/Experts/Experts.tsx
+++ landing_page/components/Experts/Experts.tsx
@@ -22,47 +22,51 @@ import { AiOutlineLinkedin } from 'react-icons/ai'
 
 export const Experts = () => {
   return (
-    <VStack>
+    <VStack bg={'#E5E5E5'}>
       <Flex
         flexDirection={'column'}
         align={'center'}
         bgImage={'/Mac.png'}
         bgSize={'cover'}
         w={'full'}
-        color={'black'}
-        h={1100}
+        color={'black'} bgPos={"center"}
+        h={'1100px'}
         pt={100}
         textStyle={'normal'}
+        justifyContent={'space-between'}
       >
-        <Text fontWeight={'bold'} textStyle={'headText'}>
-          Uzmanlar
-        </Text>
-        <Text textAlign={'center'}>
-          Uzmanlarımız, kendi alanlarında nitelikli, her türlü müşteri
-          taleplerini karşılayıp sonuçlandıracak olan ekiptir. Kendi
-          belirledikleri saat aralıklarında çalışma <br /> olanağına sahip olan
-          uzmanlarımız, sonuçlandırdıkları işlem başına bir ücret alırlar.
-          Gösterdikleri performansa göre kalite puanları da kazanan
-          uzmanlarımız,
-          <br /> kalite puanlarına bağlı olarak platformumuzdan Desk Token elde
-          ederek kendilerine ikinci bir gelir de sağlarlar.
-        </Text>
-      </Flex>
-      <Container maxW={'1200px'}>
+        <VStack>
+          <Text fontWeight={400} fontSize={'38px'}>
+            Uzmanlar
+          </Text>
+          <Text textAlign={'center'} color={'#959595'}>
+            Uzmanlarımız, kendi alanlarında nitelikli, her türlü müşteri
+            taleplerini karşılayıp sonuçlandıracak olan ekiptir. Kendi
+            belirledikleri saat aralıklarında çalışma <br /> olanağına sahip
+            olan uzmanlarımız, sonuçlandırdıkları işlem başına bir ücret
+            alırlar. Gösterdikleri performansa göre kalite puanları da kazanan
+            uzmanlarımız,
+            <br /> kalite puanlarına bağlı olarak platformumuzdan Desk Token
+            elde ederek kendilerine ikinci bir gelir de sağlarlar.
+          </Text>
+        </VStack>
         <Flex w={'full'} justifyContent={'center'}>
-          <Button bg={'#F27C00'} color={'white'} _hover={{ opacity: 0.8 }}>
+          <Button
+            bg={'#F27C00'}
+            w={'250px'}
+            color={'white'}
+            _hover={{ opacity: 0.8 }} mb={100}
+          >
             Nasıl Uzman Olurum?
           </Button>
         </Flex>
+      </Flex>
+      <Container maxW={'1200px'} py={100}>
         <VStack textStyle={'normal'}>
-          <Text
-            fontWeight={'bold'}
-            textStyle={'headText'}
-            alignSelf={'flex-start'}
-          >
+          <Text fontWeight={400} fontSize={'38px'} alignSelf={'flex-start'}>
             Team
           </Text>
-          <Text alignSelf={'flex-start'} color={'gray'}>
+          <Text alignSelf={'flex-start'} color={'#666666'} textStyle={'normal'}>
             Projemizin gelişim sürecinde aktif görev alan takım üyelerimiz.
           </Text>
           <HStack>
@@ -81,65 +85,92 @@ export const Experts = () => {
         </VStack>
         <VStack py={100}>
           <VStack w={'full'} textStyle={'normal'}>
-            <Text
-              fontWeight={'bold'}
-              textStyle={'headText'}
-              alignSelf={'flex-start'}
-            >
+            <Text fontWeight={400} fontSize={'38px'} alignSelf={'flex-start'}>
               Topluluk
             </Text>
-            <Text alignSelf={'flex-start'} color={'gray'}>
+            <Text
+              alignSelf={'flex-start'}
+              color={'#666666'}
+              textStyle={'normal'}
+            >
               Sosyal medya hesaplarımızı takip ederek son gelişmelerden haberdar
               olun.
             </Text>
           </VStack>
           <HStack gap={120} w={'full'}>
-            <HStack w={350} h={170} boxShadow={'base'} pt={25} px={5}>
+            <HStack
+              w={'384px'}
+              h={'167px'}
+              boxShadow={'base'}
+              p={'24px'}
+              bg={'white'}
+              borderRadius={10}
+            >
               <Image
                 src={'/telegram.png'}
                 alt={'telegram'}
-                w={12}
-                h={12}
+                w={'32px'}
+                h={'32px'}
                 alignSelf={'flex-start'}
               />
               <VStack alignItems={'flex-start'} alignSelf={'flex-start'}>
-                <Text fontWeight={'bold'}>Telegram Grubu</Text>
-                <Text>
-                  Telegram grubumuza katılarak diğer <br /> üyelerimizle bilgi
-                  alışverişi yapabilir, <br /> onlara soru sorabilir ve yeni
-                  sohbetler başlatabilirsiniz.
+                <Text fontWeight={500} fontSize={'15px'}>
+                  Telegram Grubu
+                </Text>
+                <Text textStyle={'normal'} color={'#525252'}>
+                  Telegram grubumuza katılarak diğer üyelerimizle bilgi
+                  alışverişi yapabilir, onlara soru sorabilir ve yeni sohbetler
+                  başlatabilirsiniz.
                 </Text>
               </VStack>
             </HStack>
 
-            <HStack w={350} h={170} boxShadow={'base'} pt={25} px={5}>
+            <HStack
+              w={'384px'}
+              h={'167px'}
+              boxShadow={'base'}
+              p={'24px'}
+              bg={'white'}
+              borderRadius={10}
+            >
               <Image
                 src={'/facebook-1.png'}
                 alt={'facebook'}
-                w={12}
-                h={12}
+                w={'32px'}
+                h={'32px'}
                 alignSelf={'flex-start'}
               />
               <VStack alignItems={'flex-start'} alignSelf={'flex-start'}>
-                <Text fontWeight={'bold'}>Facebook Grubu</Text>
-                <Text>
-                  Facebook grubumuza dahil olarak son <br /> gelişmelerden
-                  haberdar olabilirsiniz.
+                <Text fontWeight={500} fontSize={'15px'}>
+                  Facebook Grubu
+                </Text>
+                <Text textStyle={'normal'} color={'#525252'}>
+                  Facebook grubumuza dahil olarak son gelişmelerden haberdar
+                  olabilirsiniz.
                 </Text>
               </VStack>
             </HStack>
 
-            <HStack w={350} h={170} boxShadow={'base'} pt={25} px={5}>
+            <HStack
+              w={'384px'}
+              h={'167px'}
+              boxShadow={'base'}
+              p={'24px'}
+              bg={'white'}
+              borderRadius={10}
+            >
               <Image
                 src={'/discord.png'}
                 alt={'discord'}
-                w={12}
-                h={12}
+                w={'32px'}
+                h={'32px'}
                 alignSelf={'flex-start'}
               />
               <VStack alignItems={'flex-start'} alignSelf={'flex-start'}>
-                <Text fontWeight={'bold'}>Discord Grubu</Text>
-                <Text>
+                <Text fontWeight={500} fontSize={'15px'}>
+                  Discord Grubu
+                </Text>
+                <Text textStyle={'normal'} color={'#525252'}>
                   Discord grubumuza katılarak diğer <br /> üyelerimiz ile sohbet
                   ederek bilgi <br /> alışverişinde bulunabilirsiniz.
                 </Text>
@@ -155,7 +186,7 @@ export const Experts = () => {
 export default memo(Experts)
 
 interface ProfileProps {
-  ımage: string
+  Image: string
   name: string
   job: string
   link: string
@@ -164,28 +195,28 @@ interface ProfileProps {
 
 const Profile: Array<ProfileProps> = [
   {
-    ımage: '/binance.png',
+    Image: '/binance.png',
     name: 'Ahmet Hamdi Tanpınar',
     job: 'CEO and Entrepreneur',
     link: '',
     key: 'team-1'
   },
   {
-    ımage: '',
+    Image: '',
     name: 'Natalla Donald',
     job: 'CEO and Writer',
     link: '',
     key: 'team-2'
   },
   {
-    ımage: '',
+    Image: '',
     name: 'Susan Helen',
     job: 'CopyWriter',
     link: '',
     key: 'team-3'
   },
   {
-    ımage: '',
+    Image: '',
     name: 'Fatih Demirkan',
     job: 'UI & UX Designer',
     link: '',
@@ -194,7 +225,7 @@ const Profile: Array<ProfileProps> = [
 ]
 
 interface ProfileLinkProps extends FlexProps {
-  //   ımage?: string
+  Image?: string
   name: string
   job: string
   link: string
@@ -204,22 +235,18 @@ interface ProfileLinkProps extends FlexProps {
 const ProfileContent = ({ children, ...rest }: ProfileLinkProps) => (
   <Center py={6}>
     <Box
-      w={320}
-      boxShadow={'2xl'}
+      w={282}
+      h={283}
+      boxShadow={'sm'}
       rounded={'lg'}
       p={6}
       textAlign={'center'}
       color={'black'}
       transform={'auto'}
       _hover={{ scale: '1.04', opacity: '0.7', transition: '0.6s' }}
+      bg={'white'}
     >
-      <Avatar
-        size={'xl'}
-        src={''}
-        // alt={'Avatar Alt'}
-        mb={4}
-        pos={'relative'}
-      />
+      <Avatar size={'xl'} src={''} mb={4} pos={'relative'} />
       <Heading fontSize={'2xl'}>{rest.name}</Heading>
       <Text fontWeight={600} color={'gray.500'} mb={4}>
         {rest.job}
