7e1c81596605881d59d2669b0ba1b84199bba174
kursaderduhan <kursaderduhan42@gmail.com>
2022-09-20 00:34:03 +0300

    changed counter

---
+++ landing_page/components/HomePage/Counter.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect, memo} from 'react'
+import { VStack, Text, HStack, Flex } from '@chakra-ui/react'
+
+export const Counter = () => {
+  const [timerDays, setTimerDays] = useState<number>()
+  const [timerHours, setTimerHours] = useState<number>()
+  const [timerMinutes, setTimerMinutes] = useState<number>()
+  const [timerSeconds, setTimerSeconds] = useState<number>()
+
+  let interval: any
+
+  const startTimer = () => {
+    const countDownDate = new Date('September 30,2022 ').getTime()
+
+    interval = setInterval(() => {
+      const now = new Date().getTime()
+
+      const distance = countDownDate - now
+
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000))
+      const hours = Math.floor(
+        (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      )
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60))
+      const seconds = Math.floor((distance % (60 * 1000)) / 1000)
+
+      if (distance <= 0) {
+        // Stop Timer
+      } else {
+        // Update Timer
+        setTimerDays(days)
+        setTimerHours(hours)
+        setTimerMinutes(minutes)
+        setTimerSeconds(seconds)
+      }
+    })
+  }
+
+  useEffect(() => {
+    startTimer()
+  })
+
+  return (
+    <Flex >
+      <Clock
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes}
+        timerSeconds={timerSeconds}
+      />
+    </Flex>
+  )
+}
+
+export default memo(Counter)
+
+const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }: any) => {
+  return (
+    <Flex>
+      <HStack gap={2} fontSize={'4xl'}>
+        <VStack w={'full'} fontWeight={'bold'}>
+          <Text>{timerDays}</Text>
+          <Text color={'whiteAlpha.500'} fontSize={'md'}>
+            days
+          </Text>
+        </VStack>
+        <Text alignSelf={'flex-start'}>:</Text>
+        <VStack>
+          <Text>{timerHours}</Text>
+          <Text color={'whiteAlpha.500'} fontSize={'md'}>
+            hours
+          </Text>
+        </VStack>
+        <Text alignSelf={'flex-start'}>:</Text>
+        <VStack>
+          <Text>{timerMinutes}</Text>
+          <Text color={'whiteAlpha.500'} fontSize={'md'}>
+            minutes
+          </Text>
+        </VStack>
+        <Text alignSelf={'flex-start'}>:</Text>
+        <VStack>
+          <Text>{timerSeconds}</Text>
+          <Text color={'whiteAlpha.500'} fontSize={'md'}>
+            seconds
+          </Text>
+        </VStack>
+      </HStack>
+    </Flex>
+  )
+}
+
+Clock.defaultProps = {
+  timerDays: 10,
+  timerHours: 10,
+  timerMinutes: 10,
+  timerSeconds: 10
+}
--- landing_page/components/HomePage/HomePage.tsx
+++ landing_page/components/HomePage/HomePage.tsx
@@ -1,54 +1,137 @@
 import React, { memo } from 'react'
-import { HStack, VStack, Text, Button, Image, Flex,Container } from '@chakra-ui/react'
+import {
+  HStack,
+  VStack,
+  Text,
+  Button,
+  Image,
+  Flex,
+  Container,
+  Box,
+  Icon
+} from '@chakra-ui/react'
+import Counter from './Counter'
+import { RiVipDiamondLine } from 'react-icons/ri'
 
 export const Home = () => {
   return (
     <VStack
-      h={'full'} w={"full"}
+      h={'full'}
+      w={'full'}
       minW={'max-content'}
       bgImage={'/homePageBg.png'}
       bgPos={'center'}
-      bgSize={'cover'} py={50}
+      bgSize={'cover'}
+      py={50}
     >
-      <Container maxW={"1200px"} centerContent>
-      <HStack w={'full'} justifyContent={'space-between'} px={2}>
-        <VStack w={'full'} justifyContent={'flex-start'} gap={5}>
-          <VStack
-            fontSize={{ base: 30, md: 60 }}
-            justifyContent={'flex-start'}
-            w={'full'}
-          >
-            <Text w={'full'} color={'white'}>
-              Problemlerinize
+      <Container maxW={'1200px'} centerContent>
+        <HStack w={'full'} justifyContent={'space-between'} px={2}>
+          <VStack w={'full'} justifyContent={'flex-start'} gap={5}>
+            <VStack
+              fontSize={{ base: 30, md: 60 }}
+              justifyContent={'flex-start'}
+              w={'full'}
+            >
+              <Text w={'full'} color={'white'}>
+                Problemlerinize
+              </Text>
+              <HStack w={'full'}>
+                <Text color={'white'}> Anında </Text>
+                <Text color={'#F27C00'}>Çözüm</Text>
+              </HStack>
+              <Text w={'full'} color={'#F27C00'}>
+                Bulun!
+              </Text>
+            </VStack>
+            <Text alignSelf={'flex-start'} color={'white'} textStyle={'normal'}>
+              724Desk; yaşadığınız her türlü probleme günün her saatinde anlık
+              olarak,
+              <br /> farklı zaman dilimlerinde birçok dili konuşan, dünyanın en
+              iyi uzmanlarından <br /> destek alabileceğiniz bir platformdur.
             </Text>
             <HStack w={'full'}>
-              <Text color={'white'}> Anında </Text>
-              <Text color={'#F27C00'}>Çözüm</Text>
+              <Button
+                color={'#2C4EC6'}
+                w={'156px'}
+                h={'44px'}
+                bg={'white'}
+                gap={3}
+                fontSize={'13px'}
+                alignItems={'center'}
+              >
+                <Image
+                  src={'/document-text.png'}
+                  alt={'buttonIcon'}
+                  w={'18px'}
+                  h={'18px'}
+                />
+                Whitepaper
+              </Button>
+              <Image src={'36dp.png'} w={'36px'} h={'36px'} alt={'play'} />
             </HStack>
-            <Text w={'full'} color={'#F27C00'}>
-              Bulun!
-            </Text>
           </VStack>
-          <Text alignSelf={'flex-start'} color={'white'} textStyle={"normal"}> 
-            724Desk; yaşadığınız her türlü probleme günün her saatinde anlık
-            olarak,<br/> farklı zaman dilimlerinde birçok dili konuşan, dünyanın en
-            iyi uzmanlarından <br/> destek alabileceğiniz bir platformdur.
-          </Text>
-          <HStack w={'full'}>
-            <Button color={"#2C4EC6"} w={"156px"} h={"44px"} bg={"white"} gap={3} fontSize={"13px"} alignItems={"center"}> <Image src={"/document-text.png"} alt={"buttonIcon"} w={"18px"} h={"18px"}/>Whitepaper</Button>
-            <Image src={'36dp.png'} w={"36px"} h={"36px"} alt={'play'} />
+          <Image
+            src={'/1.gif'}
+            w={{ base: 200, md: 549, lg: 549, xl: 549 }}
+            h={{ base: 200, md: 549, lg: 549, xl: 549 }}
+            alt={'deskIcon'}
+          />
+        </HStack>
+        <Box
+          w={'1150px'}
+          h={'250px'}
+          bg={'#2C4EC6'}
+          rounded={15}
+          boxShadow={'dark-lg'}
+          color={'white'}
+          px={10}
+        >
+          <HStack h={'full'}>
+            <VStack
+              w='40%'
+              h={'full'}
+              justifyContent={'space-between'}
+              display={'flex'}
+              alignItems={'flex-start'}
+              py={10}
+            >
+              <Text fontSize={'2xl'}>Public Sale Schadule</Text>
+              <Counter />
+            </VStack>
+            <VStack w={'60%'} py={5}>
+              <Text alignSelf={'flex-start'} fontSize={'2xl'}>
+                18.000.000 DEX Token
+              </Text>
+              <Text alignSelf={'flex-end'}>Hard Cap</Text>
+              <Box w={'650px'} h={'20px'} bg={'#173194'} borderRadius={15}>
+                <Box w={'550px'} h={'20px'} bg={'white'} borderRadius={15} />
+              </Box>
+              <Text alignSelf={'flex-end'}>375K USD</Text>
+              <Text alignSelf={'flex-start'} fontSize={'2xl'}>
+                1 DEX = 0.10 USD
+              </Text>
+              <Button
+                color={'#2C4EC6'}
+                alignSelf='flex-end'
+                w={140}
+                alignItems={"center"} 
+              >
+                <Icon w={6} h={6} mt={1}>
+                  <RiVipDiamondLine />
+                  </Icon>
+                <Text fontSize={'sm'}>Join Whitelist</Text>
+              </Button>
+            </VStack>
           </HStack>
-        </VStack>
+        </Box>
         <Image
-          src={'/1.gif'}
-          w={{ base: 200, md: 549,lg:549,xl:549 }}
-          h={{ base: 200, md: 549,lg:549,xl:549 }}
-          alt={'deskIcon'}
+          src={'/scroll.gif'}
+          w={{ base: '40px', md: '40px', lg: '40px', xl: '40px' }}
+          h={{ base: '70px', md: '70px', lg: '70px', xl: '70px' }}
+          alt={'ıcon'}
+          mt={10}
         />
-      </HStack>
-      <Image src={'/timer.png'} alt={'homeTable'} />
-        <Image src={'/scroll.gif'} w={{ base: "40px", md: "40px", lg: "40px", xl: "40px" }} h={{ base: "70px", md: "70px", lg: "70px", xl: "70px" }} alt={'ıcon'} />
-        </Container>
+      </Container>
     </VStack>
   )
 }
