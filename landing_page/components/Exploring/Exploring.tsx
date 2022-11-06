fcfa36e9405375e6e44bca880f5f3410b0683c92
kursaderduhan <kursaderduhan42@gmail.com>
2022-11-06 03:42:38 +0300

    corrected exploring page

--- landing_page/components/Exploring/Exploring.tsx
+++ landing_page/components/Exploring/Exploring.tsx
@@ -1,14 +1,15 @@
 import React, { memo } from 'react'
 import { VStack, Button, Text } from '@chakra-ui/react'
-
+import "@fontsource/readex-pro"
 export const Exploring = () => {
   return (
-    <VStack h={600} py={100} gap={15} textStyle={"normal"} bg={"white"} color={"black"} >
-      <Text fontWeight={"bold"} fontSize={"38px"}>Keşfetmeye Başla</Text>
-      <Text textAlign={"center"} color={"#666666"} fontSize={"sm"}>
-        724Desk’in sana özel sunduğu avantajları hemen <br /> keşfetmeye başla!
+    <VStack h={600} pt={"120px"} pb={"285px"} textStyle={"normal"} bg={"white"} >
+      <Text textStyle={"resultServiceHead"} fontFamily={"Readex Pro"} color={"black"}>Keşfetmeye Başla</Text>
+      <Text textAlign={"center"} textStyle={"resultServiceText"} maxW={"448px"} pt={"4px"}>
+        724Desk’in sana özel sunduğu avantajları hemen keşfetmeye başla!
       </Text>
-      <Button bg={"#2C4EC6"} w={200} color={"white"} fontSize={"sm"} borderRadius={"8px"} _hover={{opacity:0.8}}>Keşfet</Button>
+      <Button bg={"#2C4EC6"} w={"250px"} h={"44px"} color={"white"} fontSize={"13px"} fontWeight={500} fontStyle={"normal"} lineHeight={"20px"}
+      letterSpacing={"0.5px"} borderRadius={"8px"} _hover={{ opacity: 0.8 }} top={"32px"}>Keşfet</Button>
     </VStack>
   )
 }
